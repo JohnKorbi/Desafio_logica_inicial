5da55a49bab532c62997e9a02c67b52396bb634d
JohnKorbi <jonathansousa000@gmail.com>
2023-11-28 23:10:45 -0400

    versao 1.1

--- Logica/Desafio 1/index.js
+++ Logica/Desafio 1/index.js
@@ -20,4 +20,4 @@ if (xp < 1000){
     nivel = "Radiante";
 }
 
-console.log("O nosso heroi " + heroi + "está no nível: " + nivel);
+console.log("O nosso heroi " + heroi + " está no nível: " + nivel);
